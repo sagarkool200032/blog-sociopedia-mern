cc9f9a5c2f425e51c5231c65206d118b00311134
Sagar Verma <sagarkool200032@gmail.com>
2023-08-18 04:44:48 +0530

    fixed env

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -6,7 +6,10 @@ import { setFriends } from "state";
 import FlexBetween from "./FlexBetween";
 import UserImage from "./UserImage";
 
+
+
 const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
+  const baseURL = process.env.REACT_APP_BASE_URL
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { _id } = useSelector((state) => state.user);
@@ -23,7 +26,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${_id}/${friendId}`,
+      `${process.env.REACT_APP_BASE_URL}/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
